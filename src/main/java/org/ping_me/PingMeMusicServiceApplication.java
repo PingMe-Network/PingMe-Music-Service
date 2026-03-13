eca5d4ce69e1653bf23157568d7d4a4dabb86aa8
NgocHung11 <nhpel11@gmail.com>
2026-03-14 00:12:34 +0700

    remove unused MongoDB and WebSocket configurations from application.properties and pom.xml

--- src/main/java/org/ping_me/PingMeMusicServiceApplication.java
+++ src/main/java/org/ping_me/PingMeMusicServiceApplication.java
@@ -6,15 +6,11 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditorProvider")
-@EnableMongoAuditing
 @EnableCaching
 @EnableJpaRepositories(basePackages = "org.ping_me.repository.jpa")
 @EnableFeignClients
