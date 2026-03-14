cc4e67fd5c619c2655b17240799fc8fbd1e92c60
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2026-03-14 18:07:12 +0700

    refactor(auth): Simplify AuthConfiguration and update dependencies

--- src/main/java/org/ping_me/repository/jpa/UserRepository.java
+++ src/main/java/org/ping_me/repository/jpa/UserRepository.java
@@ -1,17 +1,8 @@
 package org.ping_me.repository.jpa;
 
-import org.jspecify.annotations.NonNull;
 import org.ping_me.model.User;
-import org.ping_me.model.constant.AccountStatus;
-import org.ping_me.model.constant.UserStatus;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
