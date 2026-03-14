45f34dd99d9f93d3ae152c57e0e905d60e0a2092
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2026-03-14 23:48:15 +0700

    refactor(core): Standardize line endings in Java and POM files
    Both PingMeMusicServiceApplication.java and pom.xml files were modified to standardize line endings, changing from CRLF to LF. This change improves consistency across the codebase.

--- src/main/java/org/ping_me/PingMeMusicServiceApplication.java
+++ src/main/java/org/ping_me/PingMeMusicServiceApplication.java
@@ -3,7 +3,6 @@ package org.ping_me;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -13,7 +12,6 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 @EnableJpaAuditing(auditorAwareRef = "auditorProvider")
 @EnableCaching
 @EnableJpaRepositories(basePackages = "org.ping_me.repository.jpa")
-@EnableFeignClients
 @EnableAsync
 @EnableMethodSecurity
 public class PingMeMusicServiceApplication {
